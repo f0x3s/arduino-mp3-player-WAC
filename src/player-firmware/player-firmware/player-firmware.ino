f74cc353a6c88be3d89364d2f11d3e3c3610762a
foxes <byfoxes.design@gmail.com>
2026-07-02 13:48:55 -0500

    Total Refactor

--- src/player-firmware/player-firmware/player-firmware.ino
+++ src/player-firmware/player-firmware/player-firmware.ino
@@ -1,6 +1,14 @@
-#include <Encoder.h>
+/* 
+  audio player firmware 
+
+  created 2 July 2026
+  modified 2 July 2026
+  by foxes
+*/
+
 #include <MCUFRIEND_kbv.h>
 #include <Adafruit_GFX.h>
+#include <Encoder.h>
 
 // Color Definitions
 #define BLACK   0x0000
@@ -10,75 +18,137 @@
 #define GREEN   0x07E0
 #define GRAY    0x5AEB
 
+// Initialize TFT
 MCUFRIEND_kbv tft;
 
-// Encoder CLK to 18, DT to 19. SW to 20
-Encoder myEnc(18, 19); 
-const int buttonPin = 20;
-int buttonState = HIGH;
+// Encoder & Button Setup
+const int CLK_PIN = 18; 
+const int DT_PIN = 19;  
+const int BT_PIN = 21; 
 
-long oldPosition  = -999;
-int currentSelection = 0;
-int lastSelection = -1;
-bool inSubMenu = false;
+Encoder knob(CLK_PIN, DT_PIN);
 
-const int totalItems = 5;
-const char* menuItems[totalItems] = {
+unsigned long lastEncoderTime = 0; 
+const unsigned long dbEncoderDelay = 5000;  // Encoder Debounce (microseconds)
+unsigned long lastButtonTime = 0; 
+const unsigned long dbButtonDelay = 20000;  // Button Debounce (microseconds)
+
+bool lastButton = false;
+long lastPos = 0;
+
+// Tracklist
+const char* menuItems[] = {
   "1. Track 1",
   "2. Track 2",
   "3. Track 3",
   "4. Track 4",
   "5. Track 5"
 };
+int numMainMenuItems = sizeof(menuItems) / sizeof(menuItems[0]); 
+int mainMenuPos = 0;
+int lastMainMenuPos = 0;
+
+struct Request {
+  bool button;
+  int knob;
+};
+
+Request req = {false, 0};
+
+enum State {
+  INIT,
+  MAIN_MENU,
+  TRACK,
+};
+
+State displayState = MAIN_MENU;
+State lastDisplayState = MAIN_MENU;
 
 void setup() {
-  pinMode(buttonPin, INPUT_PULLUP);
-  //attachInterrupt(digitalPinToInterrupt(buttonPin), handleSwitch, RISING);
-  
+  // Serial.begin(115200);
+
+  pinMode(BT_PIN, INPUT_PULLUP);
+
   tft.reset();
   uint16_t ID = tft.readID();
   tft.begin(ID);
-  tft.setRotation(0); // Landscape view
-  
+  tft.setRotation(0);
+
+  drawMainMenuBG();
   drawMainMenu();
 }
 
 void loop() {
-  if (inSubMenu == false) {
-    // Read encoder position and scale down (4 ticks per physical click)
-    long newPosition = myEnc.read();
-    
-    if (newPosition != oldPosition && newPosition % 2 == 0) {
-      oldPosition = newPosition;
+  req = {false, 0};
+  checkInputs();
+
+  switch (displayState) {
+    case (MAIN_MENU) :
+      if(req.button) {
+        displayState = TRACK;
+      } else {
+        mainMenuPos += req.knob;
+        mainMenuPos = constrain(mainMenuPos, 0, numMainMenuItems-1);
+
+        if (lastDisplayState != displayState) // moving to main menu from other screen 
+        { 
+          // draw everything
+          drawMainMenuBG();
+          drawMainMenu();
+          lastDisplayState = displayState;
+        } 
+
+        else if (mainMenuPos != lastMainMenuPos) // change in highlghted menu item
+        {
+          // update selection
+          drawMainMenu();
+          lastMainMenuPos = mainMenuPos;
+        } else {
+          // no change, do nothing
+        }
+      }
       
-      // Keep selection bounded within 0 to (totalItems - 1)
-      currentSelection = newPosition % totalItems;
-      if (currentSelection < 0) currentSelection += totalItems;
-    }
+      break;
 
-    // Only redraw if the selection has changed to eliminate flicker
-    if (currentSelection != lastSelection) {
-      updateMenuHighlight();
-      lastSelection = currentSelection;
-    } 
-
-    // Check for encoder button press
-    if (digitalRead(buttonPin) == LOW) {
-      delay(200); // Simple debounce
-      executeSelection(currentSelection);
-    }
-  } else {
-    // If inside a submenu, press button again to exit back to main menu
-    if (digitalRead(buttonPin) == LOW) {
-      delay(200);
-      inSubMenu = false;
-      lastSelection = -1; // Force a menu redraw
-      drawMainMenu();
-    }
-  } 
+    case (TRACK) :
+      if (req.button) {
+        displayState = MAIN_MENU;
+      } else {
+        if (lastDisplayState != displayState) // moving to sub menu
+        { 
+          // draw everything
+          drawSubMenu();
+          lastDisplayState = displayState;
+        } 
+      }
+      break;
+  }
 }
 
-void drawMainMenu() {
+void checkInputs() {
+  unsigned long currentTime = micros();
+
+  bool currentButton = (digitalRead(BT_PIN) == LOW);
+  long currentPos = knob.read();
+
+  if (currentButton) {
+    if (currentTime - lastButtonTime > dbButtonDelay) {
+      req.button = true;
+      lastButtonTime = currentTime;
+    }
+  }
+
+  if (currentPos != lastPos && currentPos % 4 == 0) {
+    if (currentTime - lastEncoderTime > dbEncoderDelay) {
+      req.knob = (-1) * constrain(currentPos-lastPos, -1, 1);
+      lastPos = currentPos;
+      lastEncoderTime = currentTime;
+    }
+  }
+
+}
+
+void drawMainMenuBG() {
   tft.fillScreen(BLACK);
   tft.setTextColor(WHITE);
   tft.setTextSize(3);
@@ -87,16 +157,14 @@ void drawMainMenu() {
   tft.drawFastHLine(20, 55, 280, RED);
 }
 
-void updateMenuHighlight() {
+void drawMainMenu() {
   tft.setTextSize(2);
-  for (int i = 0; i < totalItems; i++) {
+  for (int i = 0; i < numMainMenuItems; i++) {
     int yPos = 80 + (i * 35); 
-    if (i == currentSelection) {
-      // Highlighted item
+    if (i == mainMenuPos) {
       tft.fillRect(15, yPos - 5, 290, 28, BLUE);
       tft.setTextColor(WHITE);
     } else {
-      // Clear background for non-highlighted items
       tft.fillRect(15, yPos - 5, 290, 28, BLACK);
       tft.setTextColor(WHITE);
     }
@@ -105,17 +173,16 @@ void updateMenuHighlight() {
   }
 }
 
-void executeSelection(int selection) {
-  inSubMenu = true;
+void drawSubMenu() {
   tft.fillScreen(BLACK);
   tft.setTextColor(GREEN);
   tft.setTextSize(3);
   tft.setCursor(20, 40);
   tft.print("Opened:\n  ");
-  tft.println(menuItems[selection]);
+  tft.println(menuItems[mainMenuPos]);
   
   tft.setTextColor(WHITE);
   tft.setTextSize(2);
   tft.setCursor(20, 180);
-  tft.println("Press Click to Return");
+  tft.println("Press Knob to Return");
 }
