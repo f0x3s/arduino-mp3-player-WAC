1c2071375f3cb5c5fc7157bf49b8f076e1757b1c
foxes <byfoxes.design@gmail.com>
2026-07-02 16:35:50 -0500

    updated test mp3s

--- src/player-firmware/testTrackDisplayNames.h
+++ src/player-firmware/testTrackDisplayNames.h
@@ -5,5 +5,10 @@ const char* menuItems[] = {
   "4. Liszt 4",
   "5. Liszt 5",
   "6. Liszt 6",
-  "7. Liszt 7"
+  "7. Liszt 7",
+  "8. Liszt 8",
+  "9. Liszt 9",
+  "10. Liszt 10"
+  "11. Liszt 11",
+  "12 Liszt 12"
 };
